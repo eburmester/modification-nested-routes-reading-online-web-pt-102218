fa2fbd0a805ff96d2d4005e85793a447f09b4587
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-12 02:40:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,9 +36,9 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  private
-
+ private
+ 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
